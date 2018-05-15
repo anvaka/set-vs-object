caa01075fcecf7b0e1e2533184874971c07f1ebd
Andriy Kashcha <anvaka@gmail.com>
2018-05-14 19:48:48 -0700

    Calling correct script
    fixes https://github.com/anvaka/set-vs-object/issues/6

--- testMemory.sh
+++ testMemory.sh
@@ -5,4 +5,4 @@ echo "Testing with Set"
 for x in $(seq 1 4); do node ./setMemoryUsage.js $KEYS_COUNT; done
 
 echo "Testing with Object"
-for x in $(seq 1 4); do node ./setMemoryUsage.js $KEYS_COUNT; done
+for x in $(seq 1 4); do node ./objectMemoryUsage.js $KEYS_COUNT; done
